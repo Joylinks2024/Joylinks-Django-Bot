10c5a557a1e1ffdf9df71a1321d9f6ee4d0c086b
Joylinks2024 <joylinkstelebot@gmail.com>
2024-04-18 20:00:57 +0500

    Joylinks TG Bot Django

--- models/views.py
+++ models/views.py
@@ -42,7 +42,7 @@ def NextTopSoreUserList(request):
         ser_data = serializer.data
         if len(ser_data) <= 9:
             ser_data = []
-        return Response(ser_data)
+        return Response(ser_data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
